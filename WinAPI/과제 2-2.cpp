4643d852b5a076f87701414b461b007a682c4b41
pollutedlake <psh950827@gmail.com>
2023-06-10 17:47:51 +0900

    애플 로고 그리기

--- WinAPI/과제 2-2.cpp
+++ WinAPI/과제 2-2.cpp
@@ -5,105 +5,121 @@
 */
 
 #include<Windows.h>
-#include<math.h>
-
-#define PI 3.141592
+#include"resource1.h"
 
 HINSTANCE _hInstance;
 HWND _hWnd;
-LPTSTR _lpszClass = TEXT("test");
+LPTSTR _lpszClass = TEXT("Windows API");
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+int APIENTRY WinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpszCmdParam,
+    int       nCmdShow)
 {
-	_hInstance = hInstance;
+    _hInstance = hInstance;
 
-	WNDCLASS wndClass;
-	wndClass.cbClsExtra = 0;
-	wndClass.cbWndExtra = 0;
-	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndClass.hInstance = hInstance;
-	wndClass.lpfnWndProc = (WNDPROC)WndProc;
-	wndClass.lpszClassName = _lpszClass;
-	wndClass.lpszMenuName = NULL;
-	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    WNDCLASS wndClass;
 
-	RegisterClass(&wndClass);
+    wndClass.cbClsExtra = 0;                                                    // 클래스 여분 메모리
+    wndClass.cbWndExtra = 0;                                                    // 윈도우 여분 메모리
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);               // 백그라운드
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);                             // 마우스 커서
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);                           // 아이콘
+    wndClass.hInstance = hInstance;                                             // 윈도우를 소요한 프로그램의 식별자 정보
+    wndClass.lpfnWndProc = (WNDPROC)WndProc;                                    // 윈도우 프로시저
+    wndClass.lpszClassName = _lpszClass;                                        // 클래스 이름 (식별자 정보)
+    wndClass.lpszMenuName = NULL;                                               // 메뉴 이름
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;                                   // 윈도우 스타일 (다시 그리기 정보)
 
-	_hWnd = CreateWindow
-	(
-		_lpszClass,
-		_lpszClass,
-		WS_OVERLAPPEDWINDOW,
-		400,
-		100,
-		800,
-		800,
-		NULL,
-		(HMENU)NULL,
-		hInstance,
-		NULL
-	);
+    RegisterClass(&wndClass);
 
-	ShowWindow(_hWnd, nCmdShow);
+    _hWnd = CreateWindow
+    (
+        _lpszClass,                     // 윈도우 클래스 식별자
+        _lpszClass,                     // 윈도우 타이틀 바 이름
+        WS_OVERLAPPEDWINDOW,            // 윈도우 스타일
+        400,                            // 윈도우 화면 X 좌표
+        100,                            // 윈도우 화면 Y 좌표
+        800,                            // 윈도우 화면 가로 크기
+        800,                            // 윈도우 화면 세로 크기
+        NULL,                           // 부모 윈도우 -> GetDsktopWindow
+        (HMENU)NULL,                    // 메뉴 핸들
+        hInstance,                      // 인스턴스 지정
+        NULL                            // 윈도우의 자식 윈도우를 생성하면 지정하고 그렇지 않다면 NULL
+                                        // ㄴ 필요에 의해서 사용하기도 하지만 지금은 NULL
+    );
 
-	MSG message;
+    ShowWindow(_hWnd, nCmdShow);
 
-	while (GetMessage(&message, 0, 0, 0))
-	{
-		TranslateMessage(&message);
-		DispatchMessage(&message);
-	}
+    MSG message;
 
-	return message.wParam;
+    while (GetMessage(&message, 0, 0, 0))
+    {
+
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+    return message.wParam;
+}
+
+void DrawBitmap(HDC hdc, int x, int y, int width, int height, HBITMAP hBit)
+{
+    HDC MemDC;
+    HBITMAP OldBitmap;
+    int bx, by;
+    BITMAP bit;
+
+    MemDC = CreateCompatibleDC(hdc);
+    OldBitmap = (HBITMAP)SelectObject(MemDC, hBit);
+
+    GetObject(hBit, sizeof(BITMAP), &bit);
+    bx = bit.bmWidth;
+    by = bit.bmHeight;
+    StretchBlt(hdc, x, y, width, height, MemDC, 0, 0, bx, by,SRCCOPY);
+
+    SelectObject(MemDC, OldBitmap);
+    DeleteDC(MemDC);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
-	HDC hdc;
-	PAINTSTRUCT ps;
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rt = { 0, 0, 256, 128 };
+    static HBITMAP MyBitmap;
+    static bool isDraw = true;
 
-	switch (iMessage)
-	{
-	case WM_CREATE:
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		Ellipse(hdc, 100, 100, 700, 700);
-		for (int i = 100; i < 700; i += 1)
-		{
-			for (int j = 100; j < 700; j += 1)
-			{
-				SetPixel(hdc, i, j, RGB(96, 187, 68));
-				SetPixel(hdc, i, j, RGB(252, 184, 39));
-				SetPixel(hdc, i, j, RGB(245, 130, 31));
-				SetPixel(hdc, i, j, RGB(224, 58, 60));
-				SetPixel(hdc, i, j, RGB(149, 61, 148));
-				SetPixel(hdc, i, j, RGB(0, 158, 221));
-			}
-		}
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
+    switch (iMessage)
+    {
+    case WM_CREATE:                 // 생성자
+        MyBitmap = LoadBitmap(_hInstance, MAKEINTRESOURCE(IDB_BITMAP1));
+        break;
 
-	return DefWindowProc(hWnd, iMessage, wParam, lParam);
-}
+    case WM_PAINT:                  // 출력에 관한 모든 것을 담당한다. (문자, 그림, 도형 등등 화면에 보이는 모든것)
+        if (isDraw)
+        {
+            hdc = BeginPaint(hWnd, &ps);
+            DrawBitmap(hdc, rt.left, rt.top, rt.right, rt.bottom, MyBitmap);
+            for (int i = rt.left; i < rt.right; i++)
+            {
+                for (int j = rt.top; j < rt.bottom; j++)
+                {
+                    SetPixel(hdc, i + 300, j, GetPixel(hdc, i, j));
+                }
+            }
+            isDraw = false;
+            InvalidateRect(hWnd, &rt, TRUE);
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:                // 소멸자
+        DeleteObject(MyBitmap);
+        PostQuitMessage(0);
+        return 0;
+    }
 
-void DrawCircle(HDC hdc, int x, int y, int radius, double startAngle, double endAngle)
-{
-	double startX, startY, endX, endY;
-	double angle = startAngle;
-	startX = x + radius * cos(angle / 180 * PI);
-	startY = y + radius * sin(angle / 180 * PI);
-	MoveToEx(hdc, startX, startY, NULL);
-	for (double angle = startAngle; angle <= endAngle; angle += 0.1)
-	{
-		LineTo(hdc, x + radius * cos(angle / 180 * PI), y + radius * sin(angle / 180 * PI));
-	}
+    return DefWindowProc(hWnd, iMessage, wParam, lParam);
 }
---
+++ WinAPI/소스.cpp
@@ -0,0 +1,121 @@
+#include<Windows.h>
+//#include<CommCtrl.h>
+#include"resource1.h"
+
+HINSTANCE g_hInst;
+HINSTANCE _hInstance;
+HWND _hWnd;
+LPTSTR _lpszClass = TEXT("Windows API");
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpszCmdParam,
+    int       nCmdShow)
+{
+    _hInstance = hInstance;
+
+    WNDCLASS wndClass;
+
+    wndClass.cbClsExtra = 0;                                                    // 클래스 여분 메모리
+    wndClass.cbWndExtra = 0;                                                    // 윈도우 여분 메모리
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);               // 백그라운드
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);                             // 마우스 커서
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);                           // 아이콘
+    wndClass.hInstance = hInstance;                                             // 윈도우를 소요한 프로그램의 식별자 정보
+    wndClass.lpfnWndProc = (WNDPROC)WndProc;                                    // 윈도우 프로시저
+    wndClass.lpszClassName = _lpszClass;                                        // 클래스 이름 (식별자 정보)
+    wndClass.lpszMenuName = NULL;                                               // 메뉴 이름
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;                                   // 윈도우 스타일 (다시 그리기 정보)
+
+    RegisterClass(&wndClass);
+
+    _hWnd = CreateWindow
+    (
+        _lpszClass,                     // 윈도우 클래스 식별자
+        _lpszClass,                     // 윈도우 타이틀 바 이름
+        WS_OVERLAPPEDWINDOW,            // 윈도우 스타일
+        400,                            // 윈도우 화면 X 좌표
+        100,                            // 윈도우 화면 Y 좌표
+        800,                            // 윈도우 화면 가로 크기
+        800,                            // 윈도우 화면 세로 크기
+        NULL,                           // 부모 윈도우 -> GetDsktopWindow
+        (HMENU)NULL,                    // 메뉴 핸들
+        hInstance,                      // 인스턴스 지정
+        NULL                            // 윈도우의 자식 윈도우를 생성하면 지정하고 그렇지 않다면 NULL
+                                        // ㄴ 필요에 의해서 사용하기도 하지만 지금은 NULL
+    );
+
+    ShowWindow(_hWnd, nCmdShow);
+
+    MSG message;
+
+    while (GetMessage(&message, 0, 0, 0))
+    {
+
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+    return message.wParam;
+}
+
+void DrawBitmap(HDC hdc, int x, int y, HBITMAP hBit)
+{
+    HDC MemDC;
+    HBITMAP OldBitmap;
+    int bx, by;
+    BITMAP bit;
+
+    MemDC = CreateCompatibleDC(hdc);
+    OldBitmap = (HBITMAP)SelectObject(MemDC, hBit);
+
+    GetObject(hBit, sizeof(BITMAP), &bit);
+    bx = bit.bmWidth;
+    by = bit.bmHeight;
+    BitBlt(hdc, x, y, bx, by, MemDC, 0, 0, NOTSRCCOPY);
+
+    SelectObject(MemDC, OldBitmap);
+    DeleteDC(MemDC);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rt = { 0, 0, 256, 256 };
+    static HBITMAP MyBitmap;
+    static bool isDraw = true;
+
+    switch (iMessage)
+    {
+    case WM_CREATE:                 // 생성자
+        MyBitmap = LoadBitmap(_hInstance, MAKEINTRESOURCE(IDB_BITMAP1));
+        break;
+
+    case WM_PAINT:                  // 출력에 관한 모든 것을 담당한다. (문자, 그림, 도형 등등 화면에 보이는 모든것)
+        if (isDraw)
+        {
+            hdc = BeginPaint(hWnd, &ps);
+            DrawBitmap(hdc, 0, 0, MyBitmap);
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    SetPixel(hdc, i + 300, j, GetPixel(hdc, i, j));
+                }
+            }
+            isDraw = false;
+            InvalidateRect(hWnd, &rt, TRUE);
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:                // 소멸자
+        DeleteObject(MyBitmap);
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
