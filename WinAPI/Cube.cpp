26894750f7a69d708098d43213fa93275807f390
pollutedlake <psh950827@naver.com>
2023-08-16 22:49:34 +0900

    add interview

--- WinAPI/Cube.cpp
+++ WinAPI/Cube.cpp
@@ -49,14 +49,13 @@ void Cube::update(void)
 	{
 		_camera.tilt += 0.01f;
 	}
-	float _ry = cos(_camera.tilt) * -20;
-	_camera.position._z = sin(_camera.tilt) * -20;
+	float _ry = 20 * cos(PI + _camera.tilt);
+	float _rz = 20 * sin(PI + _camera.tilt);
+	float _rx = _ry * cos(PI * 3 / 2 + _camera.pan);
+	_ry = _ry * sin(PI * 3 / 2 + _camera.pan);
+	_camera.position._x = _rx;
 	_camera.position._y = _ry;
-	float _rx = cos(_camera.pan) * _camera.position._x + (-sin(_camera.pan) * _camera.position._y);
-	_camera.position._y = sin(_camera.pan) * _rx + cos(_camera.pan) * _camera.position._y;
-	/*float _rx = -sin(_camera.pan) * -20;
-	_camera.position._y = cos(_camera.pan) * -20;
-	_camera.position._x = _rx;*/
+	_camera.position._z = _rz;
 	if (KEYMANAGER->isStayKeyDown(VK_UP))
 	{
 		_camera.FOV -= 0.01f;
@@ -110,6 +109,9 @@ void Cube::render(void)
 	LineMake(getMemDC(), WINSIZE_X / 2 + _projectedPoints[6]._x * 300, WINSIZE_Y / 2 + _projectedPoints[6]._y * 300, WINSIZE_X / 2 + _projectedPoints[7]._x * 300, WINSIZE_Y / 2 + _projectedPoints[7]._y * 300);
 	SelectObject(getMemDC(), oldPen);
 	DeleteObject(myPen);
+	char s[256];
+	wsprintf(s, "%f, %f, %f", _camera.position._x, _camera.position._y, _camera.position._z);
+	TextOut(getMemDC(), 0, 0, s, strlen(s));
 }
 
 void Cube::rotate()
@@ -138,25 +140,25 @@ Vector3D Cube::projectionTransform(Vector3D cameraPoint)
 	return projectedPoint;
 }
 
-//투영행렬
-Matrix4x4 Cube::getProjectionMatrix()
-{
-    //Xndc값을 구하기 위한 종횡비 
-    static float invA = (float)WINSIZE_Y / (float)WINSIZE_X;
-    //근평면과의 거리
-    static float d = 1.f / tanf(_camera.FOV * 0.5f);
-    //근평면과 카메라의 거리
-    static float n = 5.5f;
-    //원평면과 카메라의 거리
-    static float f = 1000.f;
-    //k와 l을 구하기 위한값(곱하기보다 나눗셈이 비용이 크게 드니 미리 나눠둠)
-    float invNF = 1.f / (n - f);
-    
-    float k = f * invNF;
-    float l = f * n * invNF;
-    return Matrix4x4(
-        Vector4(invA * d, 0, 0, 0),
-        Vector4(0, d, 0, 0),
-        Vector4(0.f, 0.f, k, -1.f),
-        Vector4(0.f, 0.f, l, 0.f));
-}
+////투영행렬
+//Matrix4x4 Cube::getProjectionMatrix()
+//{
+//    //Xndc값을 구하기 위한 종횡비 
+//    static float invA = (float)WINSIZE_Y / (float)WINSIZE_X;
+//    //근평면과의 거리
+//    static float d = 1.f / tanf(_camera.FOV * 0.5f);
+//    //근평면과 카메라의 거리
+//    static float n = 5.5f;
+//    //원평면과 카메라의 거리
+//    static float f = 1000.f;
+//    //k와 l을 구하기 위한값(곱하기보다 나눗셈이 비용이 크게 드니 미리 나눠둠)
+//    float invNF = 1.f / (n - f);
+//    
+//    float k = f * invNF;
+//    float l = f * n * invNF;
+//    return Matrix4x4(
+//        Vector4(invA * d, 0, 0, 0),
+//        Vector4(0, d, 0, 0),
+//        Vector4(0.f, 0.f, k, -1.f),
+//        Vector4(0.f, 0.f, l, 0.f));
+//}
